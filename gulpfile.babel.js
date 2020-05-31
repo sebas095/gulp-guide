c3ff8f2e9e7aebfd5aa65d07f2b12759bc844a42
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-05-31 03:38:57 -0500

    [ADD] cache bust + imagemin + browser sync + plumber

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -23,6 +23,18 @@ import concat from "gulp-concat";
 // Clean css
 import clean from "gulp-purgecss";
 
+// Cache bust
+import cacheBust from "gulp-cache-bust";
+
+// Optimizacion imágenes
+import imagemin from "gulp-imagemin";
+
+// Browser sync
+import { init as server, stream, reload } from "browser-sync";
+
+// Plumber
+import plumber from "gulp-plumber";
+
 // Variables/Constantes
 const cssPlugins = [cssnano(), autoprefixer()];
 const production = false;
@@ -30,6 +42,7 @@ const production = false;
 gulp.task("html-min", () => {
   return gulp
     .src("./src/*.html")
+    .pipe(plumber())
     .pipe(
       htmlmin({
         collapseWhitespace: true,
@@ -42,14 +55,17 @@ gulp.task("html-min", () => {
 gulp.task("styles", () => {
   return gulp
     .src("./src/**/*.css")
+    .pipe(plumber())
     .pipe(concat("styles.min.css"))
     .pipe(postcss(cssPlugins))
-    .pipe(gulp.dest("./public/css"));
+    .pipe(gulp.dest("./public/css"))
+    .pipe(stream());
 });
 
 gulp.task("babel", () => {
   return gulp
     .src("./src/**/*.js")
+    .pipe(plumber())
     .pipe(concat("script.min.js"))
     .pipe(babel())
     .pipe(terser())
@@ -59,9 +75,15 @@ gulp.task("babel", () => {
 gulp.task("views", () => {
   return gulp
     .src("./src/views/pages/*.pug")
+    .pipe(plumber())
     .pipe(
       pug({
         pretty: !production,
+      })
+    )
+    .pipe(
+      cacheBust({
+        type: "timestamp",
       })
     )
     .pipe(gulp.dest("./public"));
@@ -70,13 +92,16 @@ gulp.task("views", () => {
 gulp.task("sass", () => {
   return gulp
     .src("./src/scss/styles.scss")
+    .pipe(plumber())
     .pipe(sass({ outputStyle: "compressed" }))
-    .pipe(gulp.dest("./public/css"));
+    .pipe(gulp.dest("./public/css"))
+    .pipe(stream());
 });
 
 gulp.task("clean", () => {
   return gulp
     .src("./public/css/styles.css")
+    .pipe(plumber())
     .pipe(
       clean({
         content: ["./public/*.html"],
@@ -85,10 +110,27 @@ gulp.task("clean", () => {
     .pipe(gulp.dest("./public/css"));
 });
 
+gulp.task("imgmin", () => {
+  return gulp
+    .src("./src/images/*")
+    .pipe(plumber())
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 30, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 1 }),
+      ])
+    )
+    .pipe(gulp.dest("./public/images"));
+});
+
 gulp.task("default", () => {
-  // gulp.watch("./src/*.html", gulp.series("html-min"));
+  server({
+    server: "./public",
+  });
+  // gulp.watch("./src/*.html", gulp.series("html-min")).on("change", reload);;
   // gulp.watch("./src/**/*.css", gulp.series("styles"));
-  gulp.watch("./src/views/**/*.pug", gulp.series("views"));
+  gulp.watch("./src/views/**/*.pug", gulp.series("views")).on("change", reload);
   gulp.watch("./src/scss/**/*.scss", gulp.series("sass"));
-  gulp.watch("./src/**/*.js", gulp.series("babel"));
+  gulp.watch("./src/**/*.js", gulp.series("babel")).on("change", reload);
 });
